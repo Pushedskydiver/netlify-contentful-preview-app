88c8c483c90dd4482cc68c17017c324914cd2aa5
Alex Clapperton <hi@alexclapperton.co.uk>
2022-03-01 11:59:00 +0000

    ✨ Add netlify buttons to sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
 import { PlainClientAPI } from 'contentful-management';
-import { Paragraph } from '@contentful/f36-components';
 import { SidebarExtensionSDK } from '@contentful/app-sdk';
+import { Button } from "@contentful/f36-button";
+import { Stack } from "@contentful/f36-core";
 
 interface SidebarProps {
   sdk: SidebarExtensionSDK;
   cma: PlainClientAPI;
 }
 
+async function triggerBuildHook() {
+  try {
+    await fetch(`${process.env.REACT_APP_NETLIFY_BUILD_HOOK}`, {
+      method: "POST",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 const Sidebar = (props: SidebarProps) => {
-  return <Paragraph>Hello Sidebar Component</Paragraph>;
+  return (
+    <Stack flexDirection="column" spacing="spacingM">
+      <Button size="medium" isFullWidth onClick={triggerBuildHook}>
+        Build Netlify Preview
+      </Button>
+      <Button size="medium" isFullWidth as="a" href={process.env.REACT_APP_NETLIFY_URL} target="_blank">
+        Open Netlify
+      </Button>
+    </Stack>
+  );
 };
 
 export default Sidebar;
